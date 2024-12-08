8fef3474f9f51bdb74b37ec5654434f4f442832d
Holger <hadronx@live.com>
2024-12-08 14:08:33 +0300

    header with struct and functions for client

---
+++ week08_project/client.h
@@ -0,0 +1,30 @@
+// Data struct of Client
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+// attributes
+struct Client{
+    int code;
+    char name[50];
+    int age;
+    char sex;
+    char address[100];
+    double salary;
+};
+
+// methods
+void menu_clients(int *opt, struct Client *lst_clients);
+void insert_client(struct Client *c);
+int get_num_clients();
+void print_client(struct Client c);
+void print_lst_clients(struct Client *lst_clients);
+int search_client(char *search_name, struct Client *lst_clients);
+struct Client get_client(int pos, struct Client *lst_clients);
+struct Client default_client();
+void sort_by_names(struct Client *lst_clients);
+void update_client(char *search_name, struct Client *lst_clients);
+void delete_client(char *search_name, struct Client *lst_clients);
+int get_highest_salary(struct Client *lst_clients);
+
+#endif
